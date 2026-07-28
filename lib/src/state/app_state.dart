467bcfd784c4ff6f7592214736332319e269df1c
AmishPrabhu <amishsprabhu@gmail.com>
2026-07-28 16:25:10 +0530

    Fix debug signingConfig and match Web Client ID in google-services.json

--- lib/src/state/app_state.dart
+++ lib/src/state/app_state.dart
@@ -66,7 +66,7 @@ class AppState extends ChangeNotifier {
   static const _tokenKey = 'club_connect_token';
   static const _googleServerClientId = String.fromEnvironment(
     'GOOGLE_SERVER_CLIENT_ID',
-    defaultValue: '158552043080-7dc8k37ipftcqi0gqv5v56vtto76b6up.apps.googleusercontent.com',
+    defaultValue: '507618298940-j2173uqae0u940daabqghl7moq32agls.apps.googleusercontent.com',
   );
 
   final ApiClient _apiClient;
